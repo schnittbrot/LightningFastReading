5e93d04f455b6b95b55a07015059e5761977ad47
Schnittbrot <contact@augenrender.com>
2020-06-23 13:04:34 +0200

    Added App Data

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+		if (disposing && (components != null))
+		{
+		components.Dispose();
+		}
+		base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 222);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(263, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(172, 131);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Speed";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(175, 147);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 206);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(55, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Input Text";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(15, 103);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(15, 147);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Pause";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(12, 19);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(336, 46);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "CONTENT FIELD";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(377, 254);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+
+		private string InputText;
+		private string NextWord;
+
+		public Form1()
+		{
+		InitializeComponent();
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			StartDisplaying();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		PauseDisplaying();
+		}
+
+		private void StartDisplaying (){
+
+
+
+		}
+
+		private void PauseDisplaying()
+		{
+
+
+
+		}
+
+		private void GetInputText(){
+			InputText = textBox1.Text;
+		}
+
+		private void FindNextWord(){
+			NextWord = 
+
+		}
+
+	}
+}
